234b6bddaf87cafada29aab7924389a8c94b4766
abhijeet117 <abhijeetkumarsah92@gmail.com>
2025-12-19 19:15:01 +0530

    practicing react

---
+++ React-1/src/App.jsx
@@ -0,0 +1,26 @@
+const App = () => {
+  
+  return (
+    <div className="card">
+      <div className="upper">
+        <img src=""/>
+        <h2>Zenistu</h2>
+        <h3>Demon Salayer</h3>
+      </div>
+
+      <div className="social-icon">
+        <i className="ri-facebook-circle-fill"></i>
+        <i className="ri-twitter-fill"></i>
+        <i className="ri-instagram-fill"></i>
+        <i className="ri-youtube-fill"></i>
+      </div>
+
+
+    </div>
+  )
+  
+}
+
+export default App
+
+
