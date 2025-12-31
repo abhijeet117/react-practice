7b6f64329a365e9723fe00659686596e3e85e215
abhijeet117 <abhijeetkumarsah92@gmail.com>
2025-12-31 12:46:31 +0530

    fetching data from lorem piksum

--- Task-9(Card-Using-Axios)/src/App.jsx
+++ Task-9(Card-Using-Axios)/src/App.jsx
@@ -1,1 +1,27 @@
+import { useState } from "react";
+import axios from "axios";
 
+const App = () => {
+  const [allData, setallData] = useState([]);
+
+  async function onClick() {
+    let data = await axios.get("https://picsum.photos/v2/list");
+
+    setallData(data.data);
+    console.log(allData);
+  }
+
+  return (
+    <div className="p-10">
+      <button onClick={onClick} className="px-8 py-4 bg-red-500">
+        Get
+      </button>
+
+      {allData.map((elem, idx) => {
+        return <h1 key={idx}>{elem.author}</h1>;
+      })}
+    </div>
+  );
+};
+
+export default App;
