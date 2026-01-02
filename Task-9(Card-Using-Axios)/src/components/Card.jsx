110daed1ac18f9909b789fc40f77202d1c884add
abhijeet117 <abhijeetkumarsah92@gmail.com>
2026-01-02 13:59:05 +0530

    Completed Random Quote Generator using Axios & useEffect

--- Task-9(Card-Using-Axios)/src/components/Card.jsx
+++ Task-9(Card-Using-Axios)/src/components/Card.jsx
@@ -1,23 +1,39 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Card = () => {
-  const [quote, setquote] = useState("")
+  const [count, setCount] = useState(0);
+
+  function handleCount() {
+    setCount(count + 1);
+  }
+
+  useEffect(() => {
+    handleSubmit();
+    console.log("works", { count });
+  }, [count]);
+
+  const [quote, setquote] = useState("");
 
   async function handleSubmit() {
     let res = await axios.get("https://dummyjson.com/quotes/");
-    let ranIndex = Math.floor(Math.random() * 29)
-    console.log(res.data.quotes[ranIndex].quote);
-    setquote(res.data.quotes[ranIndex].quote)
+    let ranIndex = Math.floor(Math.random() * 29);
+    setquote(res.data.quotes[ranIndex].quote);
   }
 
   return (
-    <div className="border h-60 w-[90%] flex flex-col items-center justify-evenly">
+    <div className="border h-60 w-[50%] w-[90%] flex flex-col items-center justify-evenly bg-[#FBA594] rounded-xl">
       <div className="flex items-center justify-center">
-        <h1 className="text-center w-[90%]">" {quote} "</h1>
+        <h1 className="text-center w-[90%] md:text-xl font-semibold">
+          " {quote} "
+        </h1>
       </div>
-      <div className="flex gap-2"></div>
-      <button onClick={handleSubmit}>Generate</button>
+
+      <button className="bg-pink-700 rounded p-2" onClick={handleCount}>
+        Generate
+      </button>
+
+      <div className="p-4">{count}</div>
     </div>
   );
 };
