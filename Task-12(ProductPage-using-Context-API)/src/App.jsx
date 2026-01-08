9cc64709cf4b72a578f48de3d7fc9a2f34d232ae
abhijeet117 <abhijeetkumarsah92@gmail.com>
2026-01-08 16:58:35 +0530

    completed receiving data & showing on page

--- Task-12(ProductPage-using-Context-API)/src/App.jsx
+++ Task-12(ProductPage-using-Context-API)/src/App.jsx
@@ -1,9 +1,32 @@
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [RealData, setRealData] = useState([]);
 
-export default App
+  async function getData() {
+    const productData = await axios.get("https://fakestoreapi.com/products");
+    setRealData(productData.data);
+    console.log(RealData);
+  }
+
+  return (
+    <div>
+      <button onClick={getData}>Get Data</button>
+
+      <div className="webpage">
+        {RealData.map(function(elem, idx) {
+        return <a className="returning-data" href="">
+          <div className="elements">
+            <img src={elem.image}/>
+            <h2>{elem.title}</h2>
+
+          </div>
+        </a>
+      })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
